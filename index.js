3af9acd1dce558effd075baabe2af4de962cee21
Carlos Alexandre Leutz <54986289+Carlos-Alexandre-Leutz@users.noreply.github.com>
2021-02-24 10:17:18 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+var html = '<div>';
+html += '<div>';
+    for(let i = 0; i < 10; i++){
+        html += '<div >tesste</div>';
+      
+    }
+html += '</div>';
+html += '</div>';
+
+document.body.innerHTML += html;
